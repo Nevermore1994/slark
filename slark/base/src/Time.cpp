08405dad06b0d3a9916866130a2b1f993be4d2ff
Nevermore1994 <875229565@qq.com>
2023-06-19 11:15:38 +0800

    fix: nothrow replace new, and update get localtime function

--- slark/base/src/Time.cpp
+++ slark/base/src/Time.cpp
@@ -5,20 +5,31 @@
 //
 #include "Time.hpp"
 #include "date.h"
+#include <ctime>
 
 namespace Slark {
 
 Time::Timestamp Time::nowTimeStamp() {
     using namespace std::chrono;
     auto tp = time_point_cast<microseconds>(system_clock::now());
-    tp += 8h;
+    tp += Time::offsetFromUTC();
     return tp.time_since_epoch().count();
+}
+
+std::chrono::seconds Time::offsetFromUTC() {
+    static std::chrono::seconds offset = []{
+        time_t t = time(nullptr);
+        struct tm lt = {0};
+        localtime_r(&t, &lt);
+        return std::chrono::seconds(lt.tm_gmtoff);
+    }();
+    return offset;
 }
 
 std::string Time::localTime() {
     using namespace std::chrono;
     auto tp = system_clock::now();
-    tp += 8h;
+    tp += Time::offsetFromUTC();
     return date::format("%F %T", tp);
 }
 }//end namespace Slark
