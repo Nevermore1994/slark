d81058afcc4f817b55f6f182375b1d6533526491
Nevermore- <875229565@qq.com>
2023-07-03 01:19:39 +0800

    test

--- CTestTestfile.cmake
+++ CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /Users/rolf.tan/selfspace/Slark
-# Build directory: /Users/rolf.tan/selfspace/Slark
+# Source directory: /Users/rolf.tan/selfspace/slark
+# Build directory: /Users/rolf.tan/selfspace/slark
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
