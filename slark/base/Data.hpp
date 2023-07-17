00816aaefde7fbb5676c055d27fc24fcafc43da4
rolf.tan <rolf.tan@zoom.us>
2023-07-17 17:03:54 +0800

    fix warning

--- slark/base/Data.hpp
+++ slark/base/Data.hpp
@@ -15,29 +15,29 @@ namespace slark {
 struct Data {
     uint64_t capacity = 0;
     uint64_t length = 0;
-    uint8_t* data = nullptr;
+    uint8_t* rawData = nullptr;
 
     Data()
         : capacity(0)
         , length(0)
-        , data(nullptr) {
+        , rawData(nullptr) {
 
     }
 
     Data(uint8_t* d, uint64_t size)
         : capacity(size)
         , length(size)
-        , data(nullptr) {
-        data = new (std::nothrow) uint8_t[length];
-        std::copy(d, d + size + 1, data);
+        , rawData(nullptr) {
+        rawData = new (std::nothrow) uint8_t[length];
+        std::copy(d, d + size + 1, rawData);
     }
 
     explicit Data(uint64_t size)
         : capacity(size)
         , length(0)
-        , data(nullptr) {
-        data = new (std::nothrow) uint8_t[size];
-        std::fill_n(data, size, 0);
+        , rawData(nullptr) {
+        rawData = new (std::nothrow) uint8_t[size];
+        std::fill_n(rawData, size, 0);
     }
 
     ~Data() {
@@ -55,53 +55,54 @@ struct Data {
         }
         res->capacity = len;
         res->length = len;
-        res->data = new (std::nothrow) uint8_t[len];
-        std::copy(data + pos, data + pos + len, res->data);
+        res->rawData = new (std::nothrow) uint8_t[len];
+        std::copy(rawData + pos, rawData + pos + len, res->rawData);
         return res;
     }
 
     inline void release() noexcept {
-        if (data) {
-            delete [] data;
-            data = nullptr;
+        if (rawData) {
+            delete [] rawData;
+            rawData = nullptr;
         }
         length = 0;
         capacity = 0;
     }
 
+    [[maybe_unused]]
     inline void resetData() const noexcept {
-        if (data) {
-            std::fill_n(data, length, 0);
+        if (rawData) {
+            std::fill_n(rawData, length, 0);
         }
     }
 
     inline void append(const Data& d) noexcept {
         auto expectLength = length + d.length;
         if (capacity < expectLength) {
-            auto p = data;
+            auto p = rawData;
             auto len = static_cast<int64_t>(static_cast<float>(expectLength) * 1.5f);
-            data = new (std::nothrow) uint8_t[static_cast<size_t>(len)];
+            rawData = new (std::nothrow) uint8_t[static_cast<size_t>(len)];
             capacity = static_cast<uint64_t>(len);
             if (p) {
-                std::copy(p, p + length, data);
+                std::copy(p, p + length, rawData);
                 delete[] p;
             }
         }
-        std::copy(d.data, d.data + d.length, data + length);
+        std::copy(d.rawData, d.rawData + d.length, rawData + length);
         length = expectLength;
     }
 
-    inline void append(std::unique_ptr<Data> data) noexcept {
-        append(*data);
-        data.reset();
+    inline void append(std::unique_ptr<Data> appendData) noexcept {
+        append(*appendData);
+        appendData.reset();
     }
 
-    inline bool empty() const noexcept {
+    [[nodiscard]]  inline bool empty() const noexcept {
         return length == 0;
     }
 
-    inline std::string_view view() const noexcept {
-        return {reinterpret_cast<char*>(data), length};
+    [[nodiscard]] inline std::string_view view() const noexcept {
+        return {reinterpret_cast<char*>(rawData), length};
     }
 };
 
