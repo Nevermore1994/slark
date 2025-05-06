f594696ff23212dec00a9a3c308e8c9c45dafa69
Nevermore- <875229565@qq.com>
2025-05-06 23:58:34 +0800

    add texture

---
+++ src/platform/Android/sdk/src/main/cpp/Texture.cpp
@@ -0,0 +1,40 @@
+//
+// Created by Nevermore- on 2025/5/6.
+//
+
+#include "Texture.h"
+#include "Log.hpp"
+
+namespace slark {
+
+Texture::Texture(uint32_t width, uint32_t height, TextureConfig config, DataPtr data)
+    : width_(width)
+    , height_(height)
+    , config_(config) {
+    glGenTextures(1, &textureId_);
+    glBindTexture(GL_TEXTURE_2D, textureId_);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, config_.warp.s);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, config_.warp.t);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, config_.filter.min);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, config_.filter.mag);
+    glTexImage2D(GL_TEXTURE_2D, 0,config_.internalFormat,
+                 static_cast<int>(width_), static_cast<int>(height_),
+                 0, config_.format, config_.type,
+                 data ? data->rawData : nullptr);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+void Texture::updateData(DataPtr data) const noexcept {
+    if (!data) {
+        return;
+    }
+    glBindTexture(GL_TEXTURE_2D, textureId_);
+    glTexImage2D(GL_TEXTURE_2D, 0, config_.internalFormat,
+                 static_cast<int>(width_), static_cast<int>(height_),
+                 0, config_.format, config_.type,
+                 data->rawData);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+
+}
---
+++ src/platform/Android/sdk/src/main/cpp/Texture.h
@@ -0,0 +1,66 @@
+//
+// Created by Nevermore- on 2025/5/6.
+//
+
+#pragma once
+#include "BitMap.h"
+#include <EGL/egl.h>
+#include <GLES3/gl3.h>
+
+namespace slark {
+
+struct TextureConfig {
+    struct Warp {
+        GLint s = GL_CLAMP_TO_EDGE;
+        GLint t = GL_CLAMP_TO_EDGE;
+    };
+    struct Filter {
+        GLint min = GL_LINEAR;
+        GLint mag = GL_LINEAR;
+    };
+    Warp warp{};
+    Filter filter{};
+    GLint internalFormat = GL_RGBA;
+    GLenum format = GL_RGBA;
+    GLenum type = GL_UNSIGNED_BYTE;
+};
+
+class Texture {
+public:
+    Texture(uint32_t width, uint32_t height, TextureConfig config = {}, DataPtr data = nullptr);
+
+    ~Texture() {
+        if (textureId_) {
+            glDeleteTextures(1, &textureId_);
+            textureId_ = 0;
+        }
+    }
+
+    void updateData(DataPtr data) const noexcept;
+
+    [[nodiscard]] uint32_t width() const noexcept {
+        return width_;
+    }
+
+    [[nodiscard]] uint32_t height() const noexcept {
+        return height_;
+    }
+
+    [[nodiscard]] GLuint textureId() const noexcept {
+        return textureId_;
+    }
+
+    [[nodiscard]] const TextureConfig& config() const noexcept {
+        return config_;
+    }
+
+    [[nodiscard]] bool isValid() const noexcept {
+        return textureId_ != 0;
+    }
+private:
+    uint32_t width_;
+    uint32_t height_;
+    TextureConfig config_;
+    GLuint textureId_ = 0;
+};
+}
